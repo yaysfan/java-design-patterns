25295f35809f64f50bb55cca03a12c24d09f1ccf
yaysfan <yaysfan@gmail.com>
2025-10-03 18:50:25 +0900

    singleton 패턴

--- src/singleton/President.java
+++ src/singleton/President.java
@@ -1,4 +1,14 @@
 package singleton;
 
 public class President {
+
+    private static President instance = new President();
+
+    private President() {}
+
+    public static President getInstance() {
+        return instance;
+    }
+
+
 }
